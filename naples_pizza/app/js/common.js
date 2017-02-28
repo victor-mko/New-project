e138c9b7c3b33103d7cc7c52c040b79979da74d4
Viktor <viktor.mykhailenko@hyuna.bb>
2017-02-28 16:38:31 +0200

    Finished based html and css, add map

--- naples_pizza/app/js/common.js
+++ naples_pizza/app/js/common.js
@@ -1,3 +1,9 @@
-// $(function() {
-// 	$('body').fadeOut();
-// })
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.59363273, lng: 34.5453836},
+    scrollwheel: false,
+    zoom: 17
+  });
+};
+
